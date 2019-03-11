144fdc01b8c7ddef4c687606750e2ef05583a101
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-03-11 21:09:26 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -3,47 +3,43 @@
 cd $1
 make
 compile=$?
+
 if [ "$compile" -gt "0" ] 
 then
 echo "Compilation fail    Memory leaks fail       Tread race fail "
 echo    1                1                  1
-exit 7
+#exit 7
 
 
 
 else
 valgrind --leak-check=full -v ./$2&>outval.txt>&1
  grep -q "ERROR SUMMARY: 0 errors"  outval.txt
+isval=$?
 
-if [  $? -eq "0" ] ; then
-        memory=0
-else
-        memory=1
-fi
 
 valgrind --tool=helgrind ./$2&>outhel.txt>&1
 grep -q "ERROR SUMMARY: 0 errors" outhel.txt
-if [ $? -eq "0" ] ; then
-        race=0
-else
-      race=1
+ishel=$?
+
+if [ "$isval" -eq 0 ] && [ "$ishel" -eq 0 ]
+then
+#  exit 0
+    echo "Compilation pass    Memory leaks pass       Tread race pass "
+    
+elif [ "$isval" -eq 0 ] && [ "$ishel" -eq 1 ]
+then 
+ # exit 1
+      echo "Compilation pass    Memory leaks pass       Tread race fail "
+     elif [ "$isval" -eq 1 ] && [ "$ishel" -eq 0 ]
+then 
+ # exit 2
+      echo "Compilation pass    Memory leaks fail       Tread race pass "
+       
+else 
+ echo "Compilation pass    Memory leaks fail       Tread race fail "
+       #exit 3
 fi
-
-
-
-
-
-if [ $memory -eq "0" ]&&[$race -eq "0"] ; then
-      exit 0
-      elif [ $memory -eq "0" ]&&[$race -eq "1"]; then
-       exit 1
-   elif [ $memory -eq "1" ]&&[$race -eq "0"]; then
-       exit 2
-        else
-       exit 3
-      fi
-
-
 
   
 fi

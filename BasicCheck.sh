849e1307a581fb6702aa70045a1d0d812f7cc231
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-03-11 17:06:08 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,41 +1,46 @@
 #!/bin/bash
+file=$1
+running=$2
 
-cd $1
-make
+cd $file
 
-if [ $? -gt "0" ] ; then
-       echo "Compilation fail  Memory leaks FAIL   Tread race FAIL"
-        exit 7
-fi
-
-valgrind --leak-check=full --error-exitcode=1 ./$2 $@ &> outval.txt>&1
-
-if [ $? -eq "0" ] ; then
+make&>makeout.txt>&1
+iscompile=$?
+echo $compile
+if [ "$compile" -gt "0" ] 
+then
+echo "Compilation fail    Memory leaks fail        Thread race fail "
+exit 7
+else
+valgrind --leak-check=full -v ./$2&>outval.txt>&1
+ grep -q "ERROR SUMMARY: 0 errors"  outval.txt
+isval=$?
+if [  $isval -eq "0" ] ; then
         memory=0
 else
         memory=1
 fi
 
-valgrind --tool=helgrind --error-exitcode=1 ./$2 $@ &>outhel.txt>&1
-if [ $? -eq "0" ] ; then
+valgrind --tool=helgrind ./$2&>outhel.txt>&1
+grep -q "ERROR SUMMARY: 0 errors" outhel.txt
+ishel=$?
+if [ $ishel -eq "0" ] ; then
         race=0
 else
-        race=1
+      race=1
 fi
 
-
-
-
-if [ $memory -eq "0"&&$race -eq "0" ] ; then
-        echo "Compilation PASS  Memory leaks PASS      thread race PASS"
-        exit 0
-elif [ $memory -eq "1"&&$race -eq "0" ] ; then
-        echo "Compilation PASS  Memory leaks FAIL       thread race PASS"
-       exit 2
-elif [ $memory -eq "0"&&$race -eq "1" ] ; then
-        echo "Compilation PASS  Memory leaks PASS       thread race FAIL"
-       exit 1
-else
-        echo "Compilation PASS  Memory leaks FAIL       thread race FAIL"
-        exit 3
+if [$memory -eq "0" &&$race -eq "0"]; then
+exit 0
+fi
+if [$memory -eq "0" &&$race -eq "1"]; then
+exit 1
+fi
+if [$memory -eq "1" &&$race -eq "0"]; then
+exit 2
+fi
+if [$memory -eq "1" &&$race -eq "1"]; then
+exit 3
+fi
+  
 fi

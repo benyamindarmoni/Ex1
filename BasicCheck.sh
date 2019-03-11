d48a3e1840878661e00ec24ded3359167e620068
Benyamin Darmoni <48030406+benyamindarmoni@users.noreply.github.com>
2019-03-11 20:38:51 +0200

    Update BasicCheck.sh

--- BasicCheck.sh
+++ BasicCheck.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-file=$1
-running=$2
 
-cd $file
-
-make&>makeout.txt>&1
-iscompile=$?
-echo $compile
+cd $1
+make
+compile=$?
 if [ "$compile" -gt "0" ] 
 then
-echo "Compilation fail    Memory leaks fail        Thread race fail "
+echo "Compilation fail    Memory leaks fail       Tread race fail "
+echo    1                1                  1
 exit 7
+
+
+
 else
 valgrind --leak-check=full -v ./$2&>outval.txt>&1
  grep -q "ERROR SUMMARY: 0 errors"  outval.txt
-isval=$?
-if [  $isval -eq "0" ] ; then
+
+if [  $? -eq "0" ] ; then
         memory=0
 else
         memory=1
@@ -23,24 +23,27 @@ fi
 
 valgrind --tool=helgrind ./$2&>outhel.txt>&1
 grep -q "ERROR SUMMARY: 0 errors" outhel.txt
-ishel=$?
-if [ $ishel -eq "0" ] ; then
+if [ $? -eq "0" ] ; then
         race=0
 else
       race=1
 fi
 
-if [$memory -eq "0" &&$race -eq "0"]; then
-exit 0
-fi
-if [$memory -eq "0" &&$race -eq "1"]; then
-exit 1
-fi
-if [$memory -eq "1" &&$race -eq "0"]; then
-exit 2
-fi
-if [$memory -eq "1" &&$race -eq "1"]; then
-exit 3
-fi
+
+
+
+
+if [ $memory -eq "0" ]&&[$race -eq "0"] ; then
+      exit 0
+      elif [ $memory -eq "0" ]&&[$race -eq "1"]; then
+       exit 1
+   elif [ $memory -eq "1" ]&&[$race -eq "0"]; then
+       exit 2
+        else
+       exit 3
+      fi
+
+
+
   
 fi
